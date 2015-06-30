d0cd152f892a2ba7fdb619648c722200be7f96b9
Muhummad Patel <muhummad.patel@gmail.com>
2015-06-30 14:37:01 +0200

    Basic player setup. (Basic player and camera movement)

---
+++ prac3/Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerInput : MonoBehaviour {
+
+	public float speed = 6f;
+	
+	Vector3 movement;
+	Animator anim;
+	Rigidbody playerRigidbody;
+	int floorMask;
+	float camRayLength = 100f;
+
+	void Start() {
+		floorMask = LayerMask.GetMask ("Floor");
+		anim = GetComponent<Animator> ();
+		playerRigidbody = GetComponent<Rigidbody> ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	
+	void FixedUpdate() {
+		float h = Input.GetAxisRaw ("Horizontal");
+		float v = Input.GetAxisRaw ("Vertical");
+		
+		Move (h, v);
+	}
+	
+	void Move(float h, float v) {
+		movement.Set (h, 0f, v);
+		movement = movement.normalized * speed * Time.deltaTime;
+		
+		playerRigidbody.MovePosition (transform.position + movement);
+	}
+}
