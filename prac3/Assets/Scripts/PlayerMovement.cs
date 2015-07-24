5730c74726ca306c97fe62f2e6bb48ecfca689e3
Muhummad Patel <muhummad.patel@gmail.com>
2015-07-24 20:21:38 +0200

    Fix issue of player and static object collisions.

--- prac3/Assets/Scripts/PlayerMovement.cs
+++ prac3/Assets/Scripts/PlayerMovement.cs
@@ -9,23 +9,51 @@ public class PlayerMovement : MonoBehaviour {
 	Rigidbody playerRigidbody;
 	Animator playerAnimator;
 
+	bool touchingObstacle;
+
 	// Use this for initialization
 	void Start () {
 		playerRigidbody = GetComponent<Rigidbody> ();
 		playerAnimator = GetComponent<Animator> ();
+		touchingObstacle = false;
 	}
 	
 	// Update is called once per physics step
 	void FixedUpdate () {
-
-		movement = movement.normalized * speed * Time.deltaTime;
+		if (touchingObstacle) {
+			movement = Vector3.zero;
+		}
+			movement = movement.normalized * speed * Time.deltaTime;
 		
-		//Debug.Log (transform.rotation.eulerAngles.y);
-		float yOff = transform.rotation.eulerAngles.y;
-		movement = Quaternion.AngleAxis(yOff, Vector3.up) * movement;
+			//Debug.Log (transform.rotation.eulerAngles.y);
+			float yOff = transform.rotation.eulerAngles.y;
+			movement = Quaternion.AngleAxis (yOff, Vector3.up) * movement;
 		
-		playerRigidbody.MovePosition (transform.position + movement);
+			playerRigidbody.MovePosition (transform.position + movement);
+			//Debug.Log ("Move");
+		
 	}
+
+//	void OnTriggerEnter (Collider other) {
+//		if (other.gameObject.tag == "Obstacle") {
+//			Debug.Log("Player hit obstacle " + other.gameObject.name);
+////			movement = Vector3.zero;
+//			touchingObstacle = true;
+//		}
+//	}
+//
+//	void OnTriggerExit (Collider other) {
+//		if (other.gameObject.tag == "Obstacle") {
+//			Debug.Log ("Player no longer touching obstacle " + other.gameObject.name);
+//
+//			touchingObstacle = false;
+//		}
+//	}
+
+//	void OnTriggerStay (Collider other){
+//		Debug.Log ("hit " + other.gameObject.name);
+//		movement = Vector3.zero;
+//	}
 
 	public void move (float horizontal, float vertical) {
 		movement = new Vector3 (horizontal, 0.0f, vertical);
