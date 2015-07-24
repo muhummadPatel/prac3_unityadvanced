5bc4f04333cf1e5ea8b8a6fca922f09e3af71b04
Muhummad Patel <muhummad.patel@gmail.com>
2015-07-24 14:58:17 +0200

    Create 1b1Lego prefab with random material assignment script.

---
+++ prac3/Assets/Scripts/RandomMaterial.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RandomMaterial : MonoBehaviour {
+
+	public Material[] materials;
+
+	// Use this for initialization
+	void Start () {
+		int matIndex = Random.Range (0, materials.Length);
+		this.renderer.material = materials [matIndex];
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
