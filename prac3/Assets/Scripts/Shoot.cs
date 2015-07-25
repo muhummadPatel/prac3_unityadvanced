14483e2d238716fbb4be51e666342e6f20556890
Muhummad Patel <muhummad.patel@gmail.com>
2015-07-25 17:37:44 +0200

    Add line renderer component to the gun muzzle to show the trail of the bullet.

--- prac3/Assets/Scripts/Shoot.cs
+++ prac3/Assets/Scripts/Shoot.cs
@@ -4,23 +4,55 @@ using System.Collections;
 public class Shoot : MonoBehaviour {
 
 	public float range = 50.0f;
+	public float recoilTime = 0.15f;
+	public float shotLineDuration = 0.01f;
+
+	float lastShot = 0.0f;
+
+	ParticleSystem muzzleFlash;
+	LineRenderer shotLine;
 
 	// Use this for initialization
 	void Start () {
-	
+		muzzleFlash = GetComponent <ParticleSystem> ();
+		shotLine = GetComponent <LineRenderer> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetButtonUp ("Fire1")) {
-			Debug.Log ("shot");
+		lastShot += Time.deltaTime;
 
-			RaycastHit hit;
-			if (Physics.Raycast(transform.position, transform.forward, out hit, range)){
-				if (hit.collider.gameObject.tag == "Destructable") {
-					TakeDamage damage = hit.collider.gameObject.GetComponent<TakeDamage>();
-					damage.explode();
-				}			}
+		if (lastShot >= recoilTime && Input.GetButtonUp ("Fire1")) {
+			singleShot();
+			lastShot = 0.0f;
+		}
+
+		if (lastShot >= shotLineDuration) {
+			shotLine.enabled = false;
 		}
 	}
+
+	void singleShot () {
+		//Debug.Log ("shot");
+		muzzleFlash.Stop ();
+		muzzleFlash.Play ();
+
+		shotLine.enabled = true;
+		shotLine.SetPosition (0, transform.position);
+		
+		RaycastHit hit;
+		Vector3 hitPoint;
+		if (Physics.Raycast (transform.position, transform.forward, out hit, range)) {
+			hitPoint = hit.point;
+			if (hit.collider.gameObject.tag == "Destructable") {
+				TakeDamage damage = hit.collider.gameObject.GetComponent<TakeDamage> ();
+				damage.explode ();
+			}			
+		} else {
+			hitPoint = transform.position + (transform.forward * 50);
+		}
+
+		shotLine.SetPosition (1, hitPoint);
+	}
+
 }
