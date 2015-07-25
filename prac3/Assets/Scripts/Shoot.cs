fb0fb828a14f0af2340cdac420e1c0de966dd3f8
Muhummad Patel <muhummad.patel@gmail.com>
2015-07-25 16:44:26 +0200

    Implement basic shooting with new gun model.

---
+++ prac3/Assets/Scripts/Shoot.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Shoot : MonoBehaviour {
+
+	public float range = 50.0f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButtonUp ("Fire1")) {
+			Debug.Log ("shot");
+
+			RaycastHit hit;
+			if (Physics.Raycast(transform.position, transform.forward, out hit, range)){
+				if (hit.collider.gameObject.tag == "Destructable") {
+					TakeDamage damage = hit.collider.gameObject.GetComponent<TakeDamage>();
+					damage.explode();
+				}			}
+		}
+	}
+}
